f5472e857a39471eb665459ae07401ecd5a8cfe9
MaxZumbies <gabrielmax98@yahoo.com.br>
2019-09-15 15:23:46 -0300

    Ajuste do método de leitura de arquivo, ArrayList -> List

--- reconhecimentoDePadrao/src/leituraDeArquivo.java
+++ reconhecimentoDePadrao/src/leituraDeArquivo.java
@@ -1,9 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -13,38 +13,36 @@ public class leituraDeArquivo {
 
     public static void main(String[] args)
             throws FileNotFoundException /*Exceção do FileReader (File not found)*/,
-            IOException /*Exceção do readLine (In out exception)*/
-    {
-        // -- Importar o arquivo .txt --
-        // Abre o arquivo
-        FileReader arq = new FileReader("matriz5x5.txt");
+            IOException /*Exceção do readLine (In out exception)*/ {
 
-        // -- Tratar os dados do arquivo --
-        // Faz a leitura bufferizada (aperfeiçoada) dos caracteres do arquivo
-        BufferedReader arqTratado = new BufferedReader(arq);
+        // -- Definir qual arquivo será usado --
+        String arquivo = "matriz5x5.txt";
+        
+        //System.out.println(carregarArquivo());
+    }
 
+    // Método para carregar o arquivo
+    private List<String> carregarArquivo(String arquivo) {
+        
+        // Cria um objeto da classe Lista onde será armazenado os valores String
+        List<String> conteudo = new ArrayList<>();
+        
         // -- Ler as linhas do arquivo --
-        // Lê as linhas do arquivo e armazena em uma lista Float
-        ArrayList<String> linhas = new ArrayList<>();
-        String linhaArq;
-        while ((linhaArq = arqTratado.readLine()) != null) {
-            linhas.add(linhaArq);
+        try (BufferedReader arquivoBuffer = new BufferedReader(new FileReader(arquivo))) {
+            String str;
+            // Lê as linhas do arquivo e armazena na lista String
+            while ((str = arquivoBuffer.readLine()) != null) {
+                conteudo.add(str);
+            }
+            // Fecha o arquivo após a leitura
+            arquivoBuffer.close();
+            
+        } catch (IOException ioe) {
+            // Caso falhe o carregamento a mensagem de erro do sistema será exibida
+            System.out.println(ioe);
         }
-
-        // Mostra a leitura até o momento
-        for (int i = 0; i < linhas.size(); i++) {
-            System.out.println(linhas.get(i));
-        }
-
-//        // -- Converter a String linhas em um vetor float --
-//        // Divide a string em strings somente com numeros e armazena em um vetor de string
-//        String vetStr[]=linha.split(" ");
-//        
-//        // Cria um vetor de float do mesmo tamanho que o vetor de string vetStr[]
-//        float A[] = new float[vetStr.length];
-//        
-//        // Converte cada posicao do vetor de String vetStr[] para float e atribui no vetor A[]
-//        for( int i=0; i < vetStr.length;i++)
-//            A[i]=Float.parseFloat(vetStr[i]);
+        
+        // Retorna a Lista String com o conteúdo do arquivo
+        return conteudo;
     }
 }
