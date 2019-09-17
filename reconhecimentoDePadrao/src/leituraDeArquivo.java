a3450763d143ec9aa08dcf85fe51935e4ac34dd3
MaxZumbies <gabrielmax98@yahoo.com.br>
2019-09-17 02:47:30 -0300

    Adição de um método estático para exibição de uma lista String && Adição de um método para converção de uma lista String para Float (Incompleto)

--- reconhecimentoDePadrao/src/leituraDeArquivo.java
+++ reconhecimentoDePadrao/src/leituraDeArquivo.java
@@ -18,11 +18,14 @@ public class leituraDeArquivo {
         // -- Definir qual arquivo será usado --
         String arquivo = "matriz5x5.txt";
         
-        //System.out.println(carregarArquivo());
+        // Chama o método para mostrar uma lista
+        // mostraListaStr(carregarArquivo(arquivo));
+        
+        converterLista(carregarArquivo(arquivo));
     }
 
-    // Método para carregar o arquivo
-    private List<String> carregarArquivo(String arquivo) {
+    // Método estático para carregar o arquivo
+    public static List<String> carregarArquivo(String arquivo) {
         
         // Cria um objeto da classe Lista onde será armazenado os valores String
         List<String> conteudo = new ArrayList<>();
@@ -44,5 +47,36 @@ public class leituraDeArquivo {
         
         // Retorna a Lista String com o conteúdo do arquivo
         return conteudo;
+    }
+    
+    // Método estático para mostrar todo conteúdo de uma lista
+    public static void mostraListaStr(List<String> lista){
+        
+        // For each percorre toda a lista
+        for(String item : lista)
+            System.out.println(item);
+    }
+    
+    // -- Converter uma lista do tipo String para o tipo Float --
+    
+    // Método estático para converte uma lista String em Float
+    public static void converterLista(List<String> listaStr){
+        List<Float> listaFloat = new ArrayList<>();
+        
+        // For each percorre toda a lista
+        for(String linha : listaStr){
+            System.out.println(linha);
+            
+            // !! Problema para variar o tamanho, assim não percorrendo todo os arrays !!
+            
+            // Essa variável serviria para ajustar o tamanho do For each
+            int tamanho= listaStr.size();
+            
+            // Esse For each está percorrendo dois índices de cada array e os convertendo
+            for(int i = 0; i < 2; i++){
+                String linhaArray[] = linha.split(" ");
+                System.out.println(linhaArray[i]);
+            }
+        }
     }
 }
